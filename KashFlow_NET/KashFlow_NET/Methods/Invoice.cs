78a1542801d86ec31a3d384c2ba73d5027c94d87
edfarrow <3471597+edfarrow@users.noreply.github.com>
2018-11-23 13:03:54 +0000

    Add invoice.cs

---
+++ KashFlow_NET/KashFlow_NET/Methods/Invoice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KashFlow_NET
+{
+	public partial class KashFlowNet
+	{
+		public async Task<int> InvoiceCreate(Models.Invoice.Invoice invoice)
+		{
+			var rawInvoice = invoice.ToData();
+			var insertRequest = new KashFlow.InsertInvoiceRequest()
+			{
+				UserName = _username,
+				Password = _password,
+				Inv = rawInvoice
+			};
+			
+			var result = await _kashflowClient.InsertInvoiceAsync(insertRequest);
+
+			return result.InsertInvoiceResult;
+		}
+
+		public async Task<int> InvoiceLineCreate(int invoiceID, Models.Invoice.InvoiceLine invoiceLine)
+		{
+			var rawInvoice = invoiceLine.ToData();
+			var insertRequest = new KashFlow.InsertInvoiceLineRequest()
+			{
+				UserName = _username,
+				Password = _password,
+				InvLine = rawInvoice,
+				InvoiceID = invoiceID
+			};
+
+			var result = await _kashflowClient.InsertInvoiceLineAsync(insertRequest);
+			return result.InsertInvoiceLineResult;
+		}
+	}
+}
